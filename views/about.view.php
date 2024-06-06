29a41693223e95cad46a99b662e57e1e553ffd23
aasimmohamed <mhdsha29@gmail.com>
2024-06-06 16:00:17 +0530

    initial uploading

---
+++ views/about.view.php
@@ -0,0 +1,28 @@
+<html lang="en" class="h-full bg-gray-100">
+
+<head>
+    <meta charset="UTF-8">
+    <title>MVC architecture</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="h-full">
+    <div class="min-h-full">
+
+        <?php require('partials/nav.php') ?>
+
+        <header class="bg-white shadow">
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">About us</h1>
+            </div>
+        </header>
+        <main>
+            <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+                <p>Now you are on the about page.</p>
+            </div>
+        </main>
+    </div>
+
+</body>
+
+</html>
